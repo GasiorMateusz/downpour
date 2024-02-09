2fe97a587ea403addc3e5f9fc26e5a3eb3435625
GasiorMateusz <mateuszgasior314@gmail.com>
2024-02-09 22:01:53 +0100

    init project

---
+++ backend/src/main/java/com/cobra/Downpour.java
@@ -0,0 +1,12 @@
+package com.cobra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Downpour {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Downpour.class, args);
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+<h1>Welcome to downpour!</h1>
+
+It helps you keep an eye on the rain fall levels in your area.
