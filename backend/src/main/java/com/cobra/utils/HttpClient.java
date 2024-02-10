76c38e3d37213e8d2471279a27bbccb43c94b01a
GasiorMateusz <mateuszgasior314@gmail.com>
2024-02-10 21:37:13 +0100

    add HttpClient

---
+++ backend/src/main/java/com/cobra/utils/HttpClient.java
@@ -0,0 +1,31 @@
+package com.cobra.utils;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+@Slf4j
+public class HttpClient {
+
+    static CloseableHttpClient client = HttpClients.createDefault();
+
+    public static JsonObject sendGetRequest(String url) {
+        try {
+            HttpGet request = new HttpGet(url);
+            try (CloseableHttpResponse response = client.execute(request)) {
+                String jsonResponse = EntityUtils.toString(response.getEntity());
+                Gson gson = new Gson();
+                return gson.fromJson(jsonResponse, JsonObject.class);
+            }
+        } catch (Exception e) {
+            log.error("Error while sending request on url : " + url + "\n" + e);
+            return null;
+        }
+    }
+
+}
