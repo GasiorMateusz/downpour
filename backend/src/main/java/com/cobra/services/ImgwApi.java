fda0bf77ad9add39185a2f6d3bc7b32f66c0feb6
GasiorMateusz <mateuszgasior314@gmail.com>
2024-03-24 22:25:41 +0100

    implement getBasicMeasurementsForAllCities

--- backend/src/main/java/com/cobra/services/ImgwApi.java
+++ backend/src/main/java/com/cobra/services/ImgwApi.java
@@ -10,12 +10,16 @@ import org.springframework.stereotype.Service;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class ImgwApi implements WeatherDataService {
     public static final String GODZINA_POMIARU = "godzina_pomiaru";
     public static final String SUMA_OPADU = "suma_opadu";
+    public static final String STACJA = "stacja";
+
     String baseUrl = "https://danepubliczne.imgw.pl/api/data/synop/station/";
 
     @Override
@@ -38,6 +42,27 @@ public class ImgwApi implements WeatherDataService {
 
     @Override
     public List<BasicMeasurements> getBasicMeasurementsForAllCities() {
-        return null;
+        JsonArray response = Objects.requireNonNull(HttpClient.sendGetRequest(baseUrl)).getAsJsonArray();
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String currentDate = now.format(formatter);
+        List<BasicMeasurements> cities = new ArrayList<>();
+        for (JsonElement element: response
+             ) {
+            JsonObject city = element.getAsJsonObject();
+            if (city == null || !city.has(GODZINA_POMIARU) || !city.has(SUMA_OPADU)) {
+                throw new RuntimeException("No time or rainfall");
+            }
+            String dateTimeString = currentDate + "T" + city.get(GODZINA_POMIARU).getAsString() + ":00";
+            LocalDateTime timestamp = LocalDateTime.parse(dateTimeString);
+            cities.add(
+                    new BasicMeasurements(
+                            null,
+                            city.get(STACJA).getAsString(),
+                            city.get(SUMA_OPADU).getAsFloat(),
+                            Timestamp.valueOf(timestamp))
+            );
+        }
+        return cities;
     }
 }
