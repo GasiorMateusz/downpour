158bc5377ca0aa794fe03a87b342a7a3e9bb4319
GasiorMateusz <mateuszgasior314@gmail.com>
2024-03-29 14:17:26 +0100

    add User model

---
+++ backend/src/main/java/com/cobra/models/User.java
@@ -0,0 +1,17 @@
+package com.cobra.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Data
+@AllArgsConstructor
+public class User {
+
+    @Id
+    String id;
+    String name;
+
+}
